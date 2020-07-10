513f2009c7600c1a9e5dd8c1fda6ea9a27b91f29
BIswanath Sahoo <biswanath181218@gmail.com>
2020-07-10 21:21:18 +0530

    Improve performance of code

--- src/main/java/com/swayam/kbcss/request/Request.java
+++ src/main/java/com/swayam/kbcss/request/Request.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 @Data
 public class Request {
+    private String type="TUBE";
     private List<String> cycleComponents;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfPricing;
