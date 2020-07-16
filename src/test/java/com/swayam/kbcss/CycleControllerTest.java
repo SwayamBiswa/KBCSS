78ec227b1718e738880146646a81f5a4d10d230b
Biswanath Sahoo <biswanath88538@gmail.com>
2020-07-16 16:46:59 +0530

    apply test env.

--- src/test/java/com/swayam/kbcss/CycleControllerTest.java
+++ src/test/java/com/swayam/kbcss/CycleControllerTest.java
@@ -12,17 +12,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @RunWith(SpringRunner.class)
 //@ContextConfiguration(classes = {CycleController.class,CycleService.class})
 @SpringBootTest
+@ActiveProfiles("test")
 public class CycleControllerTest {
     @Autowired
     private CycleService cycleService;
